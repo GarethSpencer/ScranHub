cdbb89e4c36ffe8eec9463499b3a5de1376f74f4
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-05-02 11:43:12 +0100

    Adding LookupController to share FriendshipStatus values from the enum

---
+++ Utilities/Models/Results/FriendshipStatusResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Utilities.Models.Results;
+
+public record FriendshipStatusResult
+{
+    public required int Value { get; init; }
+    public required string Name { get; init; }
+}
---
+++ WebApi/Controllers/v1/LookupController.cs
@@ -0,0 +1,26 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Utilities.Enums;
+using Utilities.Models.Results;
+
+namespace WebApi.Controllers.v1;
+
+[ApiController]
+[Route("v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[AllowAnonymous]
+public class LookupController : ControllerBase
+{
+    private static readonly Lazy<List<FriendshipStatusResult>> Statuses = new(() =>
+        [.. Enum.GetValues<FriendshipStatus>()
+            .Cast<FriendshipStatus>()
+            .Select(s => new FriendshipStatusResult
+            {
+                Value = (int)s,
+                Name = s.ToString()
+            })]);
+
+    [HttpGet("friendship-statuses")]
+    public IActionResult GetFriendshipStatuses() => Ok(Statuses.Value);
+}
