7f718bd50738e0d00a1d7bffb475ca58b02c6a12
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-05-17 20:18:38 +0100

    Adding integration tests for SearchUsers and UpdateUsers

--- ServiceLayer.IntegrationTests/Infrastructure/UserServiceIntegrationTests.cs
+++ ServiceLayer.IntegrationTests/Infrastructure/UserServiceIntegrationTests.cs
@@ -54,6 +54,24 @@ public class UserServiceIntegrationTests(DatabaseFixture fixture) : IAsyncLifeti
 
     #region GetFriendsForUserAsync
     [Fact]
+    public async Task GetFriendsForUserAsync_NotAuthenticated_ReturnsUnauthorized()
+    {
+        _tokenData.Setup(x => x.UserId).Returns((Guid?)null);
+
+        var result = await _service!.GetFriendsForUserAsync(ct);
+        OutputFailureCheck(result, "unauthorized", "GetFriendsForUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task GetFriendsForUserAsync_InvalidUserId_ReturnsNotFound()
+    {
+        _tokenData.Setup(x => x.UserId).Returns(Guid.Empty);
+
+        var result = await _service!.GetFriendsForUserAsync(ct);
+        OutputFailureCheck(result, "not found", "GetFriendsForUserAsync", HttpStatusCode.NotFound);
+    }
+
+    [Fact]
     public async Task GetFriendsForUserAsync_User1ValidFriends_ReturnsOK()
     {
         _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
@@ -122,57 +140,9 @@ public class UserServiceIntegrationTests(DatabaseFixture fixture) : IAsyncLifeti
         typedResult.Friends?.Count().Should().Be(1);
         typedResult.FriendCount.Should().Be(1);
     }
-
-    [Fact]
-    public async Task GetFriendsForUserAsync_NotAuthenticated_ReturnsUnauthorized()
-    {
-        _tokenData.Setup(x => x.UserId).Returns((Guid?)null);
-
-        var result = await _service!.GetFriendsForUserAsync(ct);
-        OutputFailureCheck(result, "unauthorized", "GetFriendsForUserAsync", HttpStatusCode.Unauthorized);
-    }
-
-    [Fact]
-    public async Task GetFriendsForUserAsync_InvalidUserId_ReturnsNotFound()
-    {
-        _tokenData.Setup(x => x.UserId).Returns(Guid.Empty);
-
-        var result = await _service!.GetFriendsForUserAsync(ct);
-        OutputFailureCheck(result, "not found", "GetFriendsForUserAsync", HttpStatusCode.NotFound);
-    }
     #endregion
 
     #region CreateUserAsync
-    [Fact]
-    public async Task CreateUserAsync_ValidNonAdminRequest_ReturnsCreated()
-    {
-        var request = new CreateUserRequest
-        {
-            Email = "new@testemail.com",
-            Admin = false,
-            DisplayName = "New User"
-        };
-
-        var result = await _service!.CreateUserAsync(request, ct);
-        OutputSuccessCheck(result, "success", "CreateUserAsync", HttpStatusCode.Created);
-    }
-
-    [Fact]
-    public async Task CreateUserAsync_ValidAdminRequest_ReturnsCreated()
-    {
-        _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
-
-        var request = new CreateUserRequest
-        {
-            Email = "new@testemail.com",
-            Admin = true,
-            DisplayName = "New User"
-        };
-
-        var result = await _service!.CreateUserAsync(request, ct);
-        OutputSuccessCheck(result, "success", "CreateUserAsync", HttpStatusCode.Created);
-    }
-
     [Fact]
     public async Task CreateUserAsync_NotAuthenticated_ReturnsUnauthorized()
     {
@@ -235,6 +205,241 @@ public class UserServiceIntegrationTests(DatabaseFixture fixture) : IAsyncLifeti
 
         var result = await _service!.CreateUserAsync(request, ct);
         OutputFailureCheck(result, "admin", "CreateUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task CreateUserAsync_ValidNonAdminRequest_ReturnsCreated()
+    {
+        var request = new CreateUserRequest
+        {
+            Email = "new@testemail.com",
+            Admin = false,
+            DisplayName = "New User"
+        };
+
+        var result = await _service!.CreateUserAsync(request, ct);
+        OutputSuccessCheck(result, "success", "CreateUserAsync", HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task CreateUserAsync_ValidAdminRequest_ReturnsCreated()
+    {
+        _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
+
+        var request = new CreateUserRequest
+        {
+            Email = "new@testemail.com",
+            Admin = true,
+            DisplayName = "New User"
+        };
+
+        var result = await _service!.CreateUserAsync(request, ct);
+        OutputSuccessCheck(result, "success", "CreateUserAsync", HttpStatusCode.Created);
+    }
+    #endregion
+
+    #region SearchUsersAsync
+    [Fact]
+    public async Task SearchUsersAsync_NotAuthenticated_ReturnsUnauthorized()
+    {
+        _tokenData.Setup(x => x.UserId).Returns((Guid?)null);
+
+        var request = new SearchUserRequest
+        {
+            SearchText = "user",
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        var result = await _service!.SearchUsersAsync(request, ct);
+        OutputFailureCheck(result, "unauthorized", "SearchUsersAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task SearchUsersAsync_ValidSearchRequestForTwoNames_ReturnsOk()
+    {
+        var request = new SearchUserRequest
+        {
+            SearchText = "user",
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        var result = await _service!.SearchUsersAsync(request, ct);
+        OutputSuccessCheck(result, "success", "SearchUsersAsync", HttpStatusCode.OK);
+
+        var typedResult = result.Should().BeOfType<GetUsersResponse>().Subject;
+        typedResult.TotalCount.Should().Be(2);
+        typedResult.Users.Should().HaveCount(2);
+        typedResult.Users.Should().Contain(e => e.DisplayName.Equals("Admin User", StringComparison.InvariantCultureIgnoreCase));
+        typedResult.Users.Should().Contain(e => e.DisplayName.Equals("Non-Admin User", StringComparison.InvariantCultureIgnoreCase));
+
+    }
+
+    [Fact]
+    public async Task SearchUsersAsync_ValidSearchRequestForOneName_ReturnsOk()
+    {
+        var request = new SearchUserRequest
+        {
+            SearchText = "char",
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        var result = await _service!.SearchUsersAsync(request, ct);
+        OutputSuccessCheck(result, "success", "SearchUsersAsync", HttpStatusCode.OK);
+
+        var typedResult = result.Should().BeOfType<GetUsersResponse>().Subject;
+        typedResult.TotalCount.Should().Be(1);
+        typedResult.Users.Should().HaveCount(1);
+        typedResult.Users.Should().Contain(e => e.DisplayName.Equals("Charles", StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    [Fact]
+    public async Task SearchUsersAsync_ValidSearchRequestForPageSize1OfTwoNames_ReturnsOk()
+    {
+        var request = new SearchUserRequest
+        {
+            SearchText = "user",
+            PageNumber = 1,
+            PageSize = 1
+        };
+
+        var result = await _service!.SearchUsersAsync(request, ct);
+        OutputSuccessCheck(result, "success", "SearchUsersAsync", HttpStatusCode.OK);
+
+        var typedResult = result.Should().BeOfType<GetUsersResponse>().Subject;
+        typedResult.TotalCount.Should().Be(2);
+        typedResult.Users.Should().HaveCount(1);
+        typedResult.Users.Should().Contain(e => e.DisplayName.Equals("Admin User", StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    [Fact]
+    public async Task SearchUsersAsync_ValidSearchRequestForPage2OfOneName_ReturnsOk()
+    {
+        var request = new SearchUserRequest
+        {
+            SearchText = "char",
+            PageNumber = 2,
+            PageSize = 1
+        };
+
+        var result = await _service!.SearchUsersAsync(request, ct);
+        OutputSuccessCheck(result, "success", "SearchUsersAsync", HttpStatusCode.OK);
+
+        var typedResult = result.Should().BeOfType<GetUsersResponse>().Subject;
+        typedResult.TotalCount.Should().Be(1);
+        typedResult.Users.Should().HaveCount(0);
+    }
+    #endregion
+
+    #region UpdateUserAsync
+    [Fact]
+    public async Task UpdateUserAsync_NotAuthenticated_ReturnsUnauthorized()
+    {
+        _tokenData.Setup(x => x.UserId).Returns((Guid?)null);
+
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = false,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(SeedUser2NonAdminId, request, ct);
+        OutputFailureCheck(result, "unauthorized", "UpdateUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_NotAdminUpdatingSomeoneElse_ReturnsUnauthorized()
+    {
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = false,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(TestUser4NonAdminId, request, ct);
+        OutputFailureCheck(result, "update this user", "UpdateUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_NotAdminUpdatingSelfToAdmin_ReturnsUnauthorized()
+    {
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = true,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(SeedUser2NonAdminId, request, ct);
+        OutputFailureCheck(result, "make yourself an admin", "UpdateUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_UpdatingInvalidUser_ReturnsNotFound()
+    {
+        _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
+
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = false,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(Guid.Empty, request, ct);
+        OutputFailureCheck(result, "not found", "UpdateUserAsync", HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_AdminUpdatingAnotherAdmin_ReturnsUnauthorized()
+    {
+        _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
+
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = false,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(TestUser3AdminId, request, ct);
+        OutputFailureCheck(result, "update this user", "UpdateUserAsync", HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_NonAdminUpdatingSelf_ReturnsOK()
+    {
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = false,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(SeedUser2NonAdminId, request, ct);
+        OutputSuccessCheck(result, "success", "UpdateUserAsync", HttpStatusCode.OK);
+        _context!.Users.Should().ContainSingle(e => e.UserId == SeedUser2NonAdminId && e.DisplayName == "New Test User" && e.Admin == false && e.Active == true);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_AdminUpdatingNonAdmin_ReturnsOK()
+    {
+        _tokenData.Setup(x => x.UserId).Returns(SeedUser1AdminId);
+
+        var request = new UpdateUserRequest
+        {
+            DisplayName = "New Test User",
+            Admin = true,
+            Active = true
+        };
+
+        var result = await _service!.UpdateUserAsync(SeedUser2NonAdminId, request, ct);
+        OutputSuccessCheck(result, "success", "UpdateUserAsync", HttpStatusCode.OK);
+        _context!.Users.Should().ContainSingle(e => e.UserId == SeedUser2NonAdminId && e.DisplayName == "New Test User" && e.Admin == true && e.Active == true);
     }
     #endregion
 
