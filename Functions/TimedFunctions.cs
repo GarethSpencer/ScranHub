cf402a095039022bd20bb76d334248dd8f01c3f9
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-13 23:03:26 +0100

    simplifying function app to test deployment

--- Functions/TimedFunctions.cs
+++ Functions/TimedFunctions.cs
@@ -1,49 +1,20 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using RepositoryLayer.Abstractions;
-using RepositoryLayer.Abstractions.Generic;
 
 namespace Functions;
 
-public class TimedFunctions(ILoggerFactory loggerFactory, IUserRepository userRepository, IUnitOfWork unitOfWork)
+public class TimedFunctions(ILoggerFactory loggerFactory)
 {
     private readonly ILogger _logger = loggerFactory.CreateLogger<TimedFunctions>();
 
     [Function("Delete Inactive Users")]
-    public async Task Run([TimerTrigger("0 2 * * * *")] CancellationToken ct)
+    public Task Run([TimerTrigger("0 2 * * * *")] CancellationToken ct)
     {
         if (_logger.IsEnabled(LogLevel.Information))
         {
             _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.UtcNow);
         }
 
-        try
-        {
-            var inactiveUsers = await userRepository.GetAllLongTermInactiveAsync(30, ct); //users made inactive at least 30 days ago
-
-            var count = inactiveUsers.Count();
-            if (count == 0)
-            {
-                _logger.LogInformation("No inactive users to delete");
-                return;
-            }
-
-            foreach (var user in inactiveUsers)
-            {
-                await userRepository.DeleteAsync(user.UserId, ct);
-            }
-
-            await unitOfWork.SaveChangesAsync(ct);
-
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Successfully deleted {count} inactive users", count);
-            }
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Failed to delete inactive users");
-            throw;
-        }
+        return Task.CompletedTask;
     }
 }
