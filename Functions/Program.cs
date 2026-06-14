0e9fb656e65bf09e16dd1dee5d0b6f5f342149c7
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-06-14 09:32:27 +0100

    Reverting function app test changes now that the deployment issues are ressolved

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,14 +1,27 @@
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RepositoryLayer;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+if (!builder.Environment.IsDevelopment())
+{
+    var keyVaultUrl = builder.Configuration["AZURE_KEY_VAULT_URL"];
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUrl!),
+        new DefaultAzureCredential());
+}
+
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddRepositoryLayer(builder.Configuration);
 
 builder.Build().Run();
